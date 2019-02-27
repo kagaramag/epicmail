72c26d45d4785068565653a9cf739da02a8e51dc
djallas <kagaramag@gmail.com>
2019-02-27 22:47:02 +0200

    redict user on read email page

--- js/mailer.js
+++ js/mailer.js
@@ -6,13 +6,13 @@ $(function(){
       $.map(mails, function(mail, i) {
          $mailer.append('<div class="onemail">'
          +'<div class="m_profile"><img src="http://via.placeholder.com/32x32.png?text=M" alt="Profile"></div>'
-         +'<div class="m_sender">'+mail.fullnames+'</div>'
-         +'<div class="m_subject">'+mail.subject+'</div>'
+         +'<div class="m_sender truncate" onclick="readmail('+mail.id+');">'+mail.fullnames+'</div>'
+         +'<div class="m_subject truncate" onclick="readmail('+mail.id+');">'+mail.subject+'</div>'
          +'<div class="m_time">'+formatDateTime(mail.created_at)+'</div>'
          +'<div class="m_action right-align">'
          +'<a href="#"><span class="fas fa-trash-alt" title="Delete"></span></a>'
          +'<a href="#"><span class="fas fa-undo-alt" title="Retract"></span></a>'
-         +'<a href="#"><span class="fas fa-comment-alt" title="Reply"></span></a>'
+         +'<a href="#"><span class="fas fa-comment-alt" title="Instant Reply"></span></a>'
          +'</div>'
          +'<div class="clear"></div></div>');
      });      
@@ -27,4 +27,8 @@ formatDateTime = (date) => {
    var months = [ "Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec" ]
    const d = date.split('/');
    return months[d[0]-1]+" "+d[1];
+}
+
+readmail = (id) => {
+   window.location.href = 'read.html?id='+id;
 }
