9ea273455ea4a2537113430551194c4d95ee089f
djallas <kagaramag@gmail.com>
2019-02-27 22:04:39 +0200

    load mails from json

---
+++ js/mailer.js
@@ -0,0 +1,30 @@
+$(function(){
+   var url = "data/emails.json";
+   $.getJSON(url, function(data) {
+      var mails = Object.values(data);
+      var  $mailer = $('.mailer');
+      $.map(mails, function(mail, i) {
+         $mailer.append('<div class="onemail">'
+         +'<div class="m_profile"><img src="http://via.placeholder.com/32x32.png?text=M" alt="Profile"></div>'
+         +'<div class="m_sender">'+mail.fullnames+'</div>'
+         +'<div class="m_subject">'+mail.subject+'</div>'
+         +'<div class="m_time">'+formatDateTime(mail.created_at)+'</div>'
+         +'<div class="m_action right-align">'
+         +'<a href="#"><span class="fas fa-trash-alt" title="Delete"></span></a>'
+         +'<a href="#"><span class="fas fa-undo-alt" title="Retract"></span></a>'
+         +'<a href="#"><span class="fas fa-comment-alt" title="Reply"></span></a>'
+         +'</div>'
+         +'<div class="clear"></div></div>');
+     });      
+   //    $.each(mails, function (i, mail) {
+   //       $mailer.append('<div class="onemail">Email</div>');
+   //   });
+   })
+})  
+
+// format date
+formatDateTime = (date) => {
+   var months = [ "Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec" ]
+   const d = date.split('/');
+   return months[d[0]-1]+" "+d[1];
+}
