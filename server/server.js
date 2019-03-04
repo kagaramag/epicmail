8cefff9028874862231ab6b477689cf84fa2c561
djallas <kagaramag@gmail.com>
2019-03-05 01:08:43 +0200

    Refactor routes

--- server/server.js
+++ server/server.js
@@ -7,11 +7,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Parse incoming request bodies in a middleware 
+import bodyParser from 'body-parser';
 const PORT = process.env.PORT || 3000;
 
 // import routes
 import routers from './routes/index';
 app.use(routers);
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Server started with Port: ${PORT}`);
