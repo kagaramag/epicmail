a08bc266674c56a93bace6137ab64d3b608f6ec9
djallas <kagaramag@gmail.com>
2019-03-05 00:09:18 +0200

    Refactor scripts

--- server/server.js
+++ server/server.js
@@ -9,32 +9,9 @@ app.use(morgan('dev'));
 
 const PORT = process.env.PORT || 3000;
 
-
-// Register Swagger
-import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './../swagger.json';
-
-app.use('/docs', swaggerUi.serve);
-app.get('/docs', swaggerUi.setup(swaggerDocument));
-
-app.get('/', (req, res) => {
-   res.status(200).send({
-      status:200,
-      message:"Welcome to EPICMAIL"
-   })
-});
-// error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).send({
-    message: err.message,
-  });
-});
+// import routes
+import routers from './routes/index';
+app.use(routers);
 
 app.listen(PORT, () => {
   console.log(`Server started with Port: ${PORT}`);
