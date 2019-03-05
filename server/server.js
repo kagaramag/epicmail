d5dbbdbc067cfe9cb9f66fd5528c4c7d5280abed
djallas <kagaramag@gmail.com>
2019-03-05 21:48:31 +0200

    Configure handlebars for app:

--- server/server.js
+++ server/server.js
@@ -9,15 +9,40 @@ app.use(morgan('dev'));
 
 // Parse incoming request bodies in a middleware 
 import bodyParser from 'body-parser';
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 3000;
+
+import path from 'path';
+
+import  exphbs from 'express-handlebars';
+var hbs = exphbs.create({
+  helpers: {
+      foo: function () { return 'FOO!'; }
+  }
+});
+app.set("views",path.join(__dirname,"views"))
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  partialsDir: [
+    'views/includes/'
+  ]
+}));
+
+app.set('view engine', 'handlebars');
+
+// Register `hbs.engine` with the Express app.
+app.engine('handlebars', hbs.engine);
+
+
 
 // import routes
 import routers from './routes/index';
 app.use(routers);
 
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
 
 app.listen(PORT, () => {
   console.log(`Server started with Port: ${PORT}`);
