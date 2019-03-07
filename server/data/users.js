cc14600bad698aa0b3c6596a71cf2ac73dcef5ea
djallas <kagaramag@gmail.com>
2019-03-07 19:29:43 +0200

    Update data

--- server/data/users.js
+++ server/data/users.js
@@ -1,3 +1,3 @@
 const users = 
-[{"id":1,"email":"dkoeppke0@jimdo.com","firstName":"Donielle","lastName":"Koeppke","type":"admin","password":"S7EiPah7","createdOn":"2018-10-20 01:31:59","username":"dkoeppke0"},{"id":2,"email":"iwacu@epicmail.com","firstName":"Donielle","lastName":"Koeppke","type":"user","password":"$2b$15$3fNo7Yl69jb2z40L8PdqYuT5xJnOmRMOmFCyGa5qjdFq31dQ5aTVW","createdOn":"03-07-2019 04:06:57","username":"iwacu"}]
+[{"id":1,"email":"dkoeppke0@jimdo.com","firstName":"Donielle","lastName":"Koeppke","type":"admin","password":"S7EiPah7","createdOn":"2018-10-20 01:31:59","username":"dkoeppke0"},{"id":2,"email":"iwacu@epicmail.com","firstName":"Donielle","lastName":"Koeppke","type":"user","password":"$2b$15$JmIHFR74yfvn0LtcvV5QUeyux6t/t538emwouauQpvHTuULQsj6F6","createdOn":"03-07-2019 04:23:40","username":"iwacu"}]
  export default users;
