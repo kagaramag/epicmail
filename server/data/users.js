026cfe2ad6f90f2ad9730e2a27dcc79056b33c20
djallas <kagaramag@gmail.com>
2019-03-08 02:09:03 +0200

    164481865 Update user data

--- server/data/users.js
+++ server/data/users.js
@@ -1,3 +1,33 @@
-const users = 
-[{"id":1,"email":"djallas@epicmail.com","firstName":"John","lastName":"Doe","type":"user","password":"$2b$15$vAn5e2ccPbHaGrmDrM1C.eTAEhhSsJ2LNbpmGQaagvU7k/bjkxr2i","createdOn":"03-07-2019 11:50:00","username":"djallas"},{"id":2,"email":"djadja@epicmail.com","firstName":"John","lastName":"Doe","type":"user","password":"$2b$15$738llgpZY.Kzw9sdAsyw9elqGcEfbWOQyflz6kVI6kkeqYvZ1qwWa","createdOn":"03-07-2019 11:52:21","username":"djadja"},{"id":3,"email":"iwacu@epicmail.com","firstName":"Donielle","lastName":"Koeppke","type":"user","password":"$2b$15$SaRWnE0hp4KsK87wP7886.5xKIFfors52w2XKZqItqfJ4F9CIimmO","createdOn":"03-08-2019 12:05:37","username":"iwacu"}]
- export default users;
+const users = [
+  {
+    id: 1,
+    email: "djallas@epicmail.com",
+    firstName: "John",
+    lastName: "Doe",
+    type: "admin",
+    password: "$2b$15$vAn5e2ccPbHaGrmDrM1C.eTAEhhSsJ2LNbpmGQaagvU7k/bjkxr2i",
+    createdOn: "03-07-2019 11:50:00",
+    username: "djallas"
+  },
+  {
+    id: 2,
+    email: "djadja@epicmail.com",
+    firstName: "John",
+    lastName: "Doe",
+    type: "admin",
+    password: "$2b$15$738llgpZY.Kzw9sdAsyw9elqGcEfbWOQyflz6kVI6kkeqYvZ1qwWa",
+    createdOn: "03-07-2019 11:52:21",
+    username: "djadja"
+  },
+  {
+    id: 3,
+    email: "iwacu@epicmail.com",
+    firstName: "Donielle",
+    lastName: "Koeppke",
+    type: "user",
+    password: "$2b$15$SaRWnE0hp4KsK87wP7886.5xKIFfors52w2XKZqItqfJ4F9CIimmO",
+    createdOn: "03-08-2019 12:05:37",
+    username: "iwacu"
+  }
+];
+export default users;
