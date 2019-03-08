5f00d64289047dbb71ba40f4897d8adf2615a47b
djallas <kagaramag@gmail.com>
2019-03-08 02:10:42 +0200

    164481865 Add groups dummy data

---
+++ server/data/groups.js
@@ -0,0 +1,3 @@
+const groups = 
+[{"id":1,"name":"Finance","createdOn":"03-08-2019 12:44:35"},{"id":2,"name":"papy","createdOn":"03-08-2019 12:47:14"},{"id":3,"name":"papycyuro","createdOn":"03-08-2019 12:47:58"},{"id":4,"name":"amasaro","createdOn":"03-08-2019 01:41:11"},{"id":5,"name":"amasarg o","createdOn":"03-08-2019 01:41:30"},{"id":6,"name":"Fans","createdOn":"03-08-2019 02:02:11"}]
+ export default groups;
