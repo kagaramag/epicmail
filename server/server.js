b688d52eefcdcb3248d7b913982bfe056b7aae6e
djallas <kagaramag@gmail.com>
2019-03-09 22:03:30 +0200

    164487304 Add Cors module

--- server/server.js
+++ server/server.js
@@ -3,21 +3,19 @@ import express from "express";
 import morgan from "morgan";
 import "babel-polyfill";
 
+import cors from "cors";
+
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan("dev"));
-
-// allow cross origin
-// import cors from 'cors';
-// app.use(cors());
 
 // Parse incoming request bodies in a middleware
 import bodyParser from "body-parser";
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 import path from "path";
 
@@ -44,9 +42,20 @@ app.set("view engine", "handlebars");
 // Register `hbs.engine` with the Express app.
 app.engine("handlebars", hbs.engine);
 
-
 // import routes
 import routers from "./routes/index";
+
+// CORS
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.SITES);
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(routers);
 
