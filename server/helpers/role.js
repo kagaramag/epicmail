d25164b64077b638cde3df2431efcd7d763c17ab
djallas <kagaramag@gmail.com>
2019-03-14 01:13:40 +0200

    164413238 Refactor user role helper

--- server/helpers/role.js
+++ server/helpers/role.js
@@ -2,13 +2,15 @@ import jwt from "jsonwebtoken";
 
 class Role {
    // check if auth use is admin
-   static admin(request){      
-      let decoded = jwt.decode(request);
-      return decoded.type == "admin"?true:false;
+   static admin(admin){      
+      // let decoded = jwt.decode(request);
+      // return decoded.type == "admin"?true:false;
+      return admin == "admin"?true:false;
    }
-   static user(request){      
-      let decoded = jwt.decode(request);
-      return decoded.type == "user"?true:false;
+   static user(user){      
+      // let decoded = jwt.decode(request);
+      // return decoded.type == "user"?true:false;
+      return user == "user"?true:false;
    }
 }
 
