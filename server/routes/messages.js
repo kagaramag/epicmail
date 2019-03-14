6b062da97e1fdc331ab94b117537d8fe3cc267f8
djallas <kagaramag@gmail.com>
2019-03-14 18:58:13 +0200

    164638163 Add the routes for read email

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -15,6 +15,7 @@ const messageRouter = Router();
 messageRouter.post(`${version}messages`, Message.compose);
 messageRouter.get(`${version}messages`, Message.receivedEmails);
 messageRouter.get(`${version}messages/unread`, Message.unreadEmails);
+messageRouter.get(`${version}messages/read`, Message.readEmails);
 messageRouter.get(`${version}messages/sent`, Message.sentEmails);
 messageRouter.delete(`${version}messages/:id`, Message.deleteEmail);
 messageRouter.get(`${version}messages/:id`, Message.specificEmail);
