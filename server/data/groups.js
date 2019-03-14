5428082d372d3c6a7b6199c0d342b4e482f4c122
djallas <kagaramag@gmail.com>
2019-03-14 03:43:30 +0200

    164620075 Add data

--- server/data/groups.js
+++ server/data/groups.js
@@ -1,3 +1,3 @@
 const groups = 
-[{"id":1,"name":"Finance","createdOn":"03-08-2019 12:44:35"},{"id":2,"name":"papy","createdOn":"03-08-2019 12:47:14"},{"id":3,"name":"papycyuro","createdOn":"03-08-2019 12:47:58"},{"id":4,"name":"amasaro","createdOn":"03-08-2019 01:41:11"},{"id":5,"name":"amasarg o","createdOn":"03-08-2019 01:41:30"},{"id":6,"name":"Fans","createdOn":"03-08-2019 02:02:11"},{"id":7,"name":"yres","createdOn":"03-13-2019 11:21:03"}]
+[{"id":1,"name":"Finance","createdOn":"03-08-2019 12:44:35"},{"id":2,"name":"papy","createdOn":"03-08-2019 12:47:14"},{"id":3,"name":"papycyuro","createdOn":"03-08-2019 12:47:58"},{"id":4,"name":"amasaro","createdOn":"03-08-2019 01:41:11"},{"id":5,"name":"amasarg o","createdOn":"03-08-2019 01:41:30"},{"id":6,"name":"Fans","createdOn":"03-08-2019 02:02:11"},{"id":7,"name":"yres","createdOn":"03-13-2019 11:21:03"},{"id":8,"name":"FAANG","createdOn":"03-14-2019 03:36:21"}]
  export default groups;
