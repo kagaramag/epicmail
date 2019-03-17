ac19ac8c9a3e504c9ac5fb218a80304198c6f2d5
djallas <kagaramag@gmail.com>
2019-03-17 20:40:30 +0200

    164684196 Define status code

---
+++ server/config/status.js
@@ -0,0 +1,14 @@
+const ST = {
+   OK: 200,
+   CREATED: 201,
+   NO_CONTENT: 204,
+   BAD_REQUEST: 400,
+   UNAUTHORIZED: 401,
+   ACCESS_DENIED: 403,
+   NOT_FOUNT: 404,
+   TIMEOUT: 408,
+   EXIST: 409,
+   SERVER_ERROR: 500
+}
+
+export default ST
