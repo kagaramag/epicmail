28df3cb16f8cf0b1743a5a3bfd11510f9a5d2689
djallas <kagaramag@gmail.com>
2019-03-19 02:05:47 +0200

    164714467 Configure database for production

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,49 @@
 const { Pool, Client } = require('pg')
 require('dotenv').config();
 
-
+// initiate config
 let config;
-const url = require('url');
-const params = url.parse("postgres://gilles:123123@localhost:5432/epicdev");
-const auth = params.auth.split(':');
- 
-config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  ssl: false
-};
+if(process.env.NODE_ENV === 'production'){  
+  const url = require('url');
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+   
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+   
+}else if(process.env.NODE_ENV === 'test'){
+  const url = require('url');
+  const params = url.parse(process.env.TEST_DB_URL);
+  const auth = params.auth.split(':');
+  
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: process.env.SSL
+  };
+}else{
+  const url = require('url');
+  const params = url.parse(process.env.DEV_DB_URL);
+  const auth = params.auth.split(':');
+  
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: false
+  };
+}
 // console.log(config);
 const pool = new Pool(config);
 export default pool;
